1c539c56092485582016316e2635c19d0f8fd272
Kevin Cai <kevincai1979@gmail.com>
2017-08-20 14:02:21 -0700

    add login and logout function

--- frontend/index.ios.js
+++ frontend/index.ios.js
@@ -5,30 +5,66 @@ import {
   Text,
   View
 } from 'react-native';
+import firebase from 'firebase';
 import GlobalFont from 'react-native-global-font';
-import { Header, Footer } from './src/components/common';
+import { Header, Footer, ItemsList, Spinner } from './src/components/common';
+import LoginForm from './src/components/LoginForm';
 
 
 export default class CostPers extends Component {
+  state = { loggedIn: null };
 
   componentWillMount() {
-     let renogare = 'Renogare'
-     GlobalFont.applyGlobal(renogare)
+     let renogare = 'Renogare';
+     GlobalFont.applyGlobal(renogare);
+
+     firebase.initializeApp({
+       apiKey: 'AIzaSyCNqlXMbg2il8Rq-vSeHYWONM32EaYQyGc',
+       authDomain: 'costpers-50fd6.firebaseapp.com',
+       databaseURL: 'https://costpers-50fd6.firebaseio.com',
+       projectId: 'costpers-50fd6',
+       storageBucket: 'costpers-50fd6.appspot.com',
+       messagingSenderId: '121755312308'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
 
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <View>
+          <ItemsList />
+          <Footer />
+        </View>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <View>
+            <Spinner size='large' style={styles.spinnerStyle} />
+          </View>
+      );
+    }
+  }
 
   render() {
     return (
       <View style={{ flex: 1 }}>
         <Header />
-        <Text>App body</Text>
-        <Footer />
+        {this.renderContent()}
       </View>
     );
   }
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
