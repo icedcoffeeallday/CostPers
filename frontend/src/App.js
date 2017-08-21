981cc91c3ede82fd953fc850308cb5e988662dfb
Andrea Scott <andreahaleyscott@gmail.com>
2017-08-21 14:47:54 -0700

    spike design for main page, add items page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,11 @@ import {
 import firebase from 'firebase';
 import GlobalFont from 'react-native-global-font';
 import { Header, Footer, ItemsList, Spinner } from './components/common';
+import AddItem from './components/AddItem';
 import LoginForm from './components/LoginForm';
+import Main from './components/Main';
 import { Scene, Router, Actions } from 'react-native-router-flux';
+
 
 
 class App extends Component {
@@ -20,30 +23,32 @@ class App extends Component {
      let renogare = 'Renogare';
      GlobalFont.applyGlobal(renogare);
 
-     firebase.initializeApp({
-       apiKey: 'AIzaSyCNqlXMbg2il8Rq-vSeHYWONM32EaYQyGc',
-       authDomain: 'costpers-50fd6.firebaseapp.com',
-       databaseURL: 'https://costpers-50fd6.firebaseio.com',
-       projectId: 'costpers-50fd6',
-       storageBucket: 'costpers-50fd6.appspot.com',
-       messagingSenderId: '121755312308'
-    });
-
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        this.setState({ loggedIn: true });
-      } else {
-        this.setState({ loggedIn: false });
-      }
-    });
+    //  firebase.initializeApp({
+    //    apiKey: 'AIzaSyCNqlXMbg2il8Rq-vSeHYWONM32EaYQyGc',
+    //    authDomain: 'costpers-50fd6.firebaseapp.com',
+    //    databaseURL: 'https://costpers-50fd6.firebaseio.com',
+    //    projectId: 'costpers-50fd6',
+    //    storageBucket: 'costpers-50fd6.appspot.com',
+    //    messagingSenderId: '121755312308'
+    // });
+    //
+    // firebase.auth().onAuthStateChanged((user) => {
+    //   if (user) {
+    //     this.setState({ loggedIn: true });
+    //   } else {
+    //     this.setState({ loggedIn: false });
+    //   }
+    // });
   }
 
   render() {
     return (
       <Router sceneStyle={{ paddingTop: 65 }}>
         <Scene key="root">
-        <Scene key="login" component={LoginForm} title="CostPers"/>
-        <Scene key="itemslist" component={ItemsList} title="CostPers" initial/>
+          <Scene key="main" component={Main} title="test" />
+          <Scene key="login" component={LoginForm} title="CostPers"/>
+          <Scene key="itemsList" component={ItemsList} title="CostPers" />
+          <Scene key="addItem" component={AddItem} title="Add Item"/>
         </Scene>
       </Router>
     );
--- frontend/src/components/AddItem.js
+++ frontend/src/components/AddItem.js
@@ -1,14 +1,54 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 
 class AddItem extends Component {
   render() {
     return (
       <View>
-        <Text>Hello world!</Text>
+        <TextInput
+          label="Name"
+          placeholder="bike"
+          style = {styles.input}
+               placeholder = "Example: Bike"
+               placeholderTextColor = "#6A6B5F"
+               autoCapitalize = "none"
+        />
+        <TextInput
+          label="Price"
+          placeholder="Example: $100"
+          style = {styles.input}
+               placeholderTextColor = "#6A6B5F"
+        />
+        <TouchableOpacity
+          style = {styles.submitButton}>
+        <Text style = {styles.submitButtonText}> Add Item! </Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
 export default AddItem;
+
+const styles = StyleSheet.create({
+   container: {
+      paddingTop: 23
+   },
+   input: {
+      margin: 15,
+      height: 40,
+      borderColor: '#16795B',
+      borderWidth: 1,
+      padding: 10
+   },
+   submitButton: {
+      backgroundColor: '#16795B',
+      padding: 10,
+      margin: 15,
+      height: 40,
+   },
+   submitButtonText:{
+      color: 'white',
+      textAlign: 'center'
+   }
+})
