303b3ab3d62df64a292ca54365e6272e1169cedc
Faye Hayes <fhayes30@gmail.com>
2017-08-21 16:04:02 -0700

    changes to backend to make it all work

--- backend/app/controllers/uses_controller.rb
+++ backend/app/controllers/uses_controller.rb
@@ -1,12 +1,21 @@
 class UsesController < ApplicationController
+  def index
+    # @item = Item.find_by(params[:id])
+    # @uses = @item.uses
+    @uses = Use.all
+    render json: @uses
+  end
 
-  # def index
-  #   @item = Item.find(params[:item_id])
-  #   @uses = @item.uses
-  #   @costper = (@item.price / @uses.count).round
-  #   render json: @costper
-  # end
+  def new
+    @item = Item.new
+  end
 
+  def create
+    p params
+    @item = Item.find(params[:item_id])
+    @use = @item.uses.create(item_id: @item.id)
+    render json: {created: true}.to_json
+  end
 
 
 end
