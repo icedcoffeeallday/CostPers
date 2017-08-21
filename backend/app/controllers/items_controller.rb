4d0aa10331d25160576540a142c10ee74286505c
Faye Hayes <fhayes30@gmail.com>
2017-08-20 21:58:16 -0700

    Add Costper functionality to screen.

--- backend/app/controllers/items_controller.rb
+++ backend/app/controllers/items_controller.rb
@@ -2,6 +2,29 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all
-    render json: @items
+    @testitem = []
+
+      @items.each do |item|
+        newcostper = (item.price / item.uses.count).round
+        itemcp = { id: item.id.to_s + item.name, costper: newcostper}
+        @testitem << {:item => item, :costper => itemcp}
+      end
+
+    render json: @testitem
+
+    # @items.map { |item|
+    #   newcostper = (item.price / item.uses.count)
+    #   new_elm = {}
+    #   item[:costper] = newcostper
+
+    # render json: { items: @items.map { |item|
+    #   costper = (item.price / item.uses.count)
+    #   item.push(costper)
+    #   }}
+    # render json: @items
+
+
   end
+
+
 end
