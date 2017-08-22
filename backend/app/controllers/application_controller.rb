6c8931e4fffdaeddd2a1eee8389cee38da0eb944
Andrea Scott <andreahaleyscott@gmail.com>
2017-08-21 17:26:47 -0700

    Backend fixes to allow login.

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
 
   # protect_from_forgery with: :exception
   # include SessionsHelper`
