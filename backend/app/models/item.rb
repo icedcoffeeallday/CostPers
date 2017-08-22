6884cc8935c6a43218d1badc69244949a64b2ee3
Faye Hayes <fhayes30@gmail.com>
2017-08-22 12:54:20 -0700

    refactedred item model and controller. Edit itemslist to reflect this

--- backend/app/models/item.rb
+++ backend/app/models/item.rb
@@ -5,4 +5,30 @@ class Item < ApplicationRecord
 
   validates :name, :price, :user_id, presence: true
 
+  def cost_per
+    if self.uses.count > 1
+      newcostper = (self.price / self.uses.count).round
+    else
+      newcostper = self.price.round
+    end
+    newcostper
+    { id: self.id.to_s + self.name, costper: newcostper, item_id: self.id}
+
+  end
+
+  def as_json
+    {
+    id: self.id,
+    name: self.name,
+    price: self.price,
+    img_url: self.img_url,
+    star: self.star,
+    user_id: self.user_id,
+    category_id: self.category_id,
+    created_at: self.created_at,
+    updated_at: self.updated_at,
+    costper: self.cost_per
+    }
+  end
+
 end
