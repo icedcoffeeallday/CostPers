c47127e54bc1cca08a7394cdc112094c89c59f9f
Ginny Fahs <ginnyfahs@gmail.com>
2017-08-23 20:46:29 -0700

    First draft of styling for item detail page

--- frontend/src/components/ItemDetails.js
+++ frontend/src/components/ItemDetails.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
 import { Card } from './common';
+import Icon from 'react-native-vector-icons/MaterialIcons' ;
 
 class ItemDetails extends Component {
   constructor(props) {
@@ -33,33 +34,146 @@ class ItemDetails extends Component {
       console.log(response);
     })
     .catch((error) => console.log(error));
-  }
+  };
+
+  // const toggleStar=toggle?"star":"star-border";
 
   render() {
     return (
-      <View>
-        <Card>
-          <Text>{this.state.itemName}</Text>
-        </Card>
+      <View style={styles.container}>
 
+        <View style={styles.itemNameContainer}>
         <Card>
-          <Text>{this.state.uses} uses</Text>
+          <Text style={styles.itemNameText}>{this.state.itemName}</Text>
         </Card>
+        </View>
 
-        <Card>
-          <Text>Original price: ${this.state.price}</Text>
-        </Card>
+        <View style={styles.timesUsedContainer}>
+          <Card>
+            <Text style={styles.timesUsedText}>Times used: </Text>
+            <Text style={styles.timesUsedValue}>{this.state.uses}</Text>
+          </Card>
+        </View>
 
-        <Card>
-          <Text>Star</Text>
-        </Card>
+        <View style={styles.priceContainer}>
+          <Card>
+            <Text style={styles.priceText}>Price: </Text><Text style={styles.priceValue}>$ {this.state.price}</Text>
+          </Card>
+        </View>
 
-        <Card>
-          <Text>Current cost per use: ${this.state.costPer}</Text>
-        </Card>
+        <View style={styles.currentCostperContainer}>
+          <Card>
+            <Text style={styles.currentCostperText}>Current</Text>
+            <Text style={styles.costperWord}> costper </Text> 
+            <Text style={styles.currentCostperText}>use: </Text> 
+            <Text style={styles.currentCostperValue}> ${this.state.costPer}</Text>
+          </Card>
+        </View>
+
+        <View style={styles.starContainer}>
+            <Card>
+              <Icon 
+              name="star-border"
+              size={50}
+              style={{color: 'white'}}
+              />
+              <Text style={styles.starText}>Star this item</Text>
+            </Card>
+        </View>
+
       </View>
     );
   }
 }
+
+const styles= ({
+  container: {
+
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  itemNameContainer: {
+    justifyContent: 'center'
+  },
+  itemNameText: {
+    backgroundColor: 'red',
+    fontSize: 55,
+    marginTop: 60,
+    marginRight: 15,
+    marginLeft: 15,
+    height: 70,
+    width: 250,
+    color: '#16795B',
+    textAlign: 'center',
+  },
+  timesUsedContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+    marginTop: 30,
+    backgroundColor: 'orange',
+    width: 300,
+    height: 70
+  },
+  timesUsedText: {
+    color: 'black',
+    fontSize: 24,
+    width: 200,
+    height: 50,
+    backgroundColor: 'yellow'
+  },
+  timesUsedValue: {
+    color: '#16795B',
+    fontSize: 32,
+    width: 50,
+    height: 50,
+    backgroundColor: 'red'
+  },
+  priceContainer: {
+    flexDirection: 'row',
+    marginTop: 30,
+    justifyContent: 'center',
+    backgroundColor: 'yellow',
+    width: 200,
+    height: 50
+  },
+  priceText: {
+    color: 'black',
+    fontSize: 24,
+    width: 100,
+    backgroundColor: 'blue'
+  },
+  priceValue: {
+    color: '#16795B',
+    width: 60,
+    fontSize: 24, 
+    backgroundColor: 'purple'
+  },
+  currentCostperContainer: {
+    flexDirection: 'row',
+    marginTop: 30,
+    fontSize: 20,
+    backgroundColor: 'blue'
+  },
+  currentCostperText: {
+    fontSize: 20
+  },
+  costperWord: {
+    fontSize: 20,
+    color: '#16795B'
+  },
+  currentCostperValue: {
+    fontSize: 44,
+    color: '#16795B'
+  },
+  starContainer: {
+    marginTop: 30,
+    backgroundColor: 'rgb(255, 202, 58)'
+  },
+  starText: {
+    color: 'white',
+    borderRadius: 5
+  }
+})
 
 export default ItemDetails;
