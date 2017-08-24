71dc2f45bddc3203e54110b4ae3972c0da8702db
Faye Hayes <fhayes30@gmail.com>
2017-08-24 14:51:21 -0700

    Update all axios links to reflect heroku routes. Update item#create to default false for star

--- frontend/src/components/common/Costper.js
+++ frontend/src/components/common/Costper.js
@@ -16,7 +16,7 @@ class Costper extends Component {
 
 addUse(arg) {
   var myCostper = this
-    axios.post("http://localhost:3000/items/"+arg+"/uses", {
+    axios.post("https://sheltered-peak-36785.herokuapp.com/items/"+arg+"/uses", {
       item_id: arg
     })
     .then(function(response) {
