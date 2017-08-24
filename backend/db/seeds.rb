cde08b68a01e1bceb1db2201d2ca7271eecfc30a
Faye Hayes <fhayes30@gmail.com>
2017-08-24 10:45:41 -0700

    reseed database

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -2,22 +2,40 @@ User.destroy_all
 Item.destroy_all
 Use.destroy_all
 
-User.create!({
-  first_name: "Test",
-  last_name: "Tester",
-  email: "test@test.com",
-  password: "password"
-  })
 
-10.times do
-  Item.create!({
-    name: Faker::Hipster.word,
-    price: rand(50.0..450.0),
-    user_id: User.all.sample.id,
-    })
-end
+User.create!({first_name: "Faye", last_name: "Hayes", email: "faye@mail.com", password: "password"})
+User.create!({first_name: "Andrea", last_name: "Scott", email: "andrea@mail.com", password: "password"})
+User.create!({first_name: "Tyler", last_name: "Kawabata", email: "tyler@mail.com", password: "password"})
+User.create!({first_name: "Ginny", last_name: "Fahs", email: "ginny@mail.com", password: "password"})
+User.create!({first_name: "Kevin", last_name: "Cai", email: "kevin@mail.com", password: "password"})
 
-50.times do
+boolean = [true, false].sample
+
+Item.create!({name: "Coloring Book", price: 40, user_id: User.all.sample.id, star: boolean })
+Item.create!({name: "Bike", price: 500, user_id: User.all.sample.id, star: boolean })
+Item.create!({name: "iPhone", price: 800, user_id: User.all.sample.id, star: boolean })
+Item.create!({name: "MacBook", price: 1500, user_id: User.all.sample.id, star: boolean })
+Item.create!({name: "Guitar", price: 1000, user_id: User.all.sample.id, star: boolean })
+Item.create!({name: "RV", price: 50000, user_id: User.all.sample.id, star: boolean })
+Item.create!({name: "Smart TV", price: 2000, user_id: User.all.sample.id, star: boolean })
+Item.create!({name: "Shirt", price: 200, user_id: User.all.sample.id, star: boolean })
+Item.create!({name: "Speakers", price: 400, user_id: User.all.sample.id, star: boolean })
+Item.create!({name: "Tent", price: 350, user_id: User.all.sample.id, star: boolean })
+Item.create!({name: "Sony A9 Camera", price: 4000, user_id: User.all.sample.id, star: boolean })
+Item.create!({name: "Suitcase", price: 100, user_id: User.all.sample.id, star: boolean })
+Item.create!({name: "Netflix/year", price: 95.88, user_id: User.all.sample.id, star: boolean })
+Item.create!({name: "Treadmill", price: 5000, user_id: User.all.sample.id, star: boolean })
+Item.create!({name: "Cake Mixer", price: 250, user_id: User.all.sample.id, star: boolean })
+Item.create!({name: "Fitbit", price: 150, user_id: User.all.sample.id, star: boolean })
+Item.create!({name: "Messenger Bag", price: 500, user_id: User.all.sample.id, star: boolean })
+Item.create!({name: "Deep Fryer", price: 500, user_id: User.all.sample.id, star: boolean })
+Item.create!({name: "XBox", price: 300, user_id: User.all.sample.id, star: boolean })
+Item.create!({name: "Health Insurance", price: 6000, user_id: User.all.sample.id, star: boolean })
+
+
+
+
+250.times do
   Use.create!({
     item_id: Item.all.sample.id
     })
