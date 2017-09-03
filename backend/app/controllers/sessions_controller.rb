4c73dfd7b3e73e3304ac544416f5ea9a5932cf78
TylerK64 <tylerkawabata@gmail.com>
2017-09-02 18:44:33 -0700

    Rename original sessions_controller.rb to sessions_controller2.rb; create new blank sessions_controller.rb file for managing new sessions logic.

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,30 +1,3 @@
-class SessionsController < ApplicationController
-  protect_from_forgery with: :null_session
-
-  def new
-  end
-
-  def create
-    p params
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      render json: {
-        user_id: user.id,
-        first_name: user.first_name,
-        last_name: user.last_name,
-        auth: true
-      }
-    else
-      render json: {
-        user_id: null,
-        errors: ["Incorrect email or password combination."],
-        auth: false
-      }.to_json
-    end
-  end
-
-  def destroy
-    log_out
-    redirect_to root_url
-  end
+class SessionsController < ApiController
+  
 end
